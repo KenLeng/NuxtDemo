651fef776417b65ccfbe2ad6fda43855ea097485
KenLeng <k6951985@gmail.com>
2021-12-21 18:34:17 +0800

    [feat]調整header

--- components/web/header.vue
+++ components/web/header.vue
@@ -9,10 +9,16 @@
                 關於
                 <i class="far fa-info-circle"></i>
             </a>
-            <a class="nav-item" href="javascript:void(0)" @click="$router.push('/web/products')">
-                商品款式
-                <i class="fas fa-rings-wedding"></i>
-            </a>
+            <div class="nav-item dropdown">
+                商品款式<i class="fas fa-rings-wedding"></i>
+                <div class="dropdown-content">
+                    <div v-for="(data, index) in categories"
+                        :key="index"
+                        @click="redirectProduct(data.category)">
+                        <span>{{ data.name }}</span>
+                    </div>
+                </div>
+            </div>
             <a class="nav-item" href="javascript:void(0)" @click="$router.push('/web/info')">
                 店鋪資訊
                 <i class="fas fa-map-marker-alt"></i>
@@ -28,5 +34,32 @@
 <script>
 export default {
     name: 'header',
+    data() {
+        return {
+            categories: [
+                {
+                    name: '項鍊',
+                    category: 'necklace',
+                }, 
+                { 
+                    name: '戒指',
+                    category: 'ring'
+                },
+                {
+                    name: '黃金',
+                    category: 'gold'
+                }
+            ]
+        }
+    },
+    methods: {
+        redirectProduct(category) {
+            console.log('category', category)
+            this.$router.push({
+                name: 'web-products',
+                params: category
+            })
+        }
+    }
 }
 </script>
--- pages/web/products.vue
+++ pages/web/products.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
         商品款式
+        {{ $route.params }}
     </div>
 </template>
 
 <script>
 export default {
     name: 'products',
+    data() {
+        return {
+            category: 'all'
+        }
+    },
+    mounted() {
+        console.log('hahahahah')
+    }
 }
 </script>
