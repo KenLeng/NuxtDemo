8f2b9808cacd84c089bd16bc219547546e3d0f3e
Ken <k6951985@gmail.com>
2022-03-05 18:34:52 +0800

    [feat]新增套件-vue-awesome-swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "@/plugins/axios",
+    { src: "@/plugins/swiper.js", mode: 'client' },
     { src: '@/plugins/vue-fb-customer-chat', ssr: false }
   ],
 
