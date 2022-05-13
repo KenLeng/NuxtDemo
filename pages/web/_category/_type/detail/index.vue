1265016489adca7ceba092de49ee783e2fc3fc47
KenLeng <k6951985@gmail.com>
2022-05-13 18:12:11 +0800

    [feat]小切版商品明細

--- pages/web/_category/_type/detail/index.vue
+++ pages/web/_category/_type/detail/index.vue
@@ -2,9 +2,16 @@
     <div class="content-wrap">
         <div class="content">
             <div>
-                <div>
-                    <div></div>
-                    <div></div>
+                <div class="">黃金 > 戒指 > 黃金戒指（男）</div>
+                <div style="display: flex; justify-content: space-evenly;">
+                    <div>
+                        <img width="400" src="@/assets/images/demo1.png" alt="image"/>
+                    </div>
+                    <div>
+                        <div>黃金戒指（男）</div>
+                        <div>重量 : 100g</div>
+                        <div>介紹介紹介紹介紹介紹介紹</div>
+                    </div>
                 </div>
             </div>
         </div>
