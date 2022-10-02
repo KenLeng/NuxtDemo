581778e7044af190be7e73f5cafca239d7c6ed43
Ken <k6951985@gmail.com>
2022-10-03 00:44:42 +0800

    [feat] 更新img權杖

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const imgConfig = {
     img_url: 'https://zhenblob.blob.core.windows.net/picture',
-    img_expiration: 'sp=r&st=2022-10-02T07:56:21Z&se=2022-10-02T15:56:21Z&spr=https&sv=2021-06-08&sr=c&sig=DAjjbHTAwvtvKxokCC64qYPjRARgUGFVTDH%2BaeYv2y4%3D'
+    img_expiration: 'sp=r&st=2022-10-02T16:41:57Z&se=2052-10-03T00:41:57Z&spr=https&sv=2021-06-08&sr=c&sig=OF43nH0Sm6c3zH2eLaMHAZ43AatX%2FJZIVec7DRdBIdI%3D'
 }
